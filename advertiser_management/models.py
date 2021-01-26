f2dd7e7739e2657f80c2647b7722e613bcfe68ac
mohamadamin karami <mohamadaminkarami80@gmail.com>
2021-01-26 12:09:42 +0330

    define models of app

--- advertiser_management/models.py
+++ advertiser_management/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
+
 # Create your models here.
+class BaseAdvertising(models.Model):
+    clicks = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+
+    class Meta:
+        abstract = True
+
+
+class Advertiser(BaseAdvertising):
+    name = models.CharField(max_length=200)
+
+    def inc_clicks(self):
+        self.clicks += 1
+        self.save()
+        return
+
+    def inc_views(self):
+        self.views += 1
+        self.save()
+        return
+
+
+class Ad(BaseAdvertising):
+    advertiser = models.ForeignKey(Advertiser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='ads/', default='default.jpg')
+    link = models.URLField()
+
+    def inc_clicks(self):
+        self.clicks += 1
+        self.save()
+        self.advertiser.inc_clicks()
+        return
+
+    def inc_views(self):
+        self.views += 1
+        self.save()
+        self.advertiser.inc_views()
+        return
